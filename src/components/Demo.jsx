7cb06e3a6fc6394766c6b7e806bde8348706e900
gurxt <jung_jungian@outlook.com>
2023-06-19 21:43:44 -0400

    modified logo and added link svg

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,11 +1,47 @@
-import logo from '../assets/logo.png'
 
-// #5d2525
-// #b20000
+import { useState, useEffect } from 'react'
+import link from '../assets/link.svg'
 
 const Demo = () => {
-  return (
-    <div></div>
+    const [article, setArticle] = useState({
+        url: '',
+        summary: ''
+    })
+
+    const handleSubmit = async (e) => {
+        alert("Submitted")
+    }
+
+    return (
+        <section className="mt-16 w-full max-w-xl">
+            <div className="flex flex-col w-full gap-2">
+                <form
+                    className="relative flex justify-center items-center"
+                    onSubmit={handleSubmit}
+                >
+                    <img 
+                        src={link}
+                        alt="link_icon"
+                        className="absolute left-0 my-2 ml-3 w-5"
+                    />
+                    <input
+                        type="url"
+                        placeholder="Enter a url"
+                        value={article.url}
+                        onChange={(e) => setArticle({ ...article, url: e.target.value })}
+                        required 
+                        className="url_input peer"
+                    />
+                    <button
+                        type="submit"
+                        className="submit_btn peer-focus:border-gray-700 peer-foucs:text-gray-700"
+                    >
+                    </button>
+                </form>
+                {/* Browse URL History */}
+                {/* Disply Results */}
+            </div>
+        </section>
   )
 }
 
