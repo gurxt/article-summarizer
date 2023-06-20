07061b1e5d130f0d7f28641fc2beac491cbafeb1
gurxt <jung_jungian@outlook.com>
2023-06-19 21:43:33 -0400

    configured redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Application from './Application'
+import { Provider } from 'react-redux'
+import { store } from './services/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Application />
-    </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <Application />
+        </React.StrictMode>
+    </Provider>
 )
